f91ec2a384e1ae6bc5dd2ce048340f1fcdd9ed52
IanGuilherme <80356689+IanGuilherme@users.noreply.github.com>
2022-07-13 15:36:26 -0300

    Continuação das rotas. Fixed #16

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,16 @@
 from django.urls import path
-from .views import HomeView, QuemSomosView
+from .views import HomeView, QuemSomosView, CatequeseInfantilView, EucaristiaView, PerseverancaView, CrismaView, ProjetosView, EventosView, InscricoesView, InscricoesCatequizandosView, InscricoesCatequistasView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('quem-somos', QuemSomosView.as_view(), name='quem somos')
+    path('quem-somos', QuemSomosView.as_view(), name='quem somos'),
+    path('catequese-infantil', CatequeseInfantilView.as_view(), name='catequese infantil'),
+    path('eucaristia', EucaristiaView.as_view(), name='eucaristia'),
+    path('perseveranca', PerseverancaView.as_view(), name='perseveranca'),
+    path('crisma', CrismaView.as_view(), name='crisma'),
+    path('projetos', ProjetosView.as_view(), name='projetos'),
+    path('eventos', EventosView.as_view(), name='eventos'),
+    path('inscricoes', InscricoesView.as_view(), name='inscricoes'),
+    path('inscricoes-catequizandos', InscricoesCatequizandosView.as_view(), name='inscricoes catequizandos'),
+    path('inscricoes-catequistas', InscricoesCatequistasView.as_view(), name='inscricoes catequistas'),
 ]
--- base/views.py
+++ base/views.py
@@ -7,3 +7,31 @@ class HomeView(TemplateView):
 
 class QuemSomosView(TemplateView):
     template_name= 'QuemSomos.html'
+
+class CatequeseInfantilView(TemplateView):
+    template_name= 'CatequeseInfantil.html'
+
+class EucaristiaView(TemplateView):
+    template_name= 'Eucaristia.html'
+
+class PerseverancaView(TemplateView):
+    template_name= 'Perseveranca.html'
+
+class CrismaView(TemplateView):
+    template_name= 'Crisma.html'
+
+class ProjetosView(TemplateView):
+    template_name= 'Projetos.html'
+
+class EventosView(TemplateView):
+    template_name= 'Eventos.html'
+
+class InscricoesView(TemplateView):
+    template_name= 'Inscricoes.html'
+
+class InscricoesCatequistasView(TemplateView):
+    template_name= 'InscricoesCatequistas.html'
+
+class InscricoesCatequizandosView(TemplateView):
+    template_name= 'InscricoesCatequizandos.html'
+
